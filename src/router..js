91a1a7bb09e47e1f1de5e4faf177e59f0b9f31a4
jialinming <1142091339@qq.com>
2018-09-12 16:18:44 +0800

    模块搭建

--- src/router..js
+++ src/router..js
@@ -1,11 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from './components/app/login.vue';
+import Reg from './components/app/reg.vue';
+
 
 import store from './store'
 Vue.use(Router)
 
 
-const router=new Router()
+const router = new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/reg',
+            name: 'reg',
+            component: reg
+        },{
+            path: '/manager',
+            name: 'manager',
+            component: manager
+        }
+    ]
+})
 
 
 export default router;
