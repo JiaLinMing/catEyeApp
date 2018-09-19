0a894d262ab8833ec33eca18e674dd0c2c966b4d
panwenqihappy <panxiaoqihappy@163.com>
2018-09-19 10:35:52 +0800

    添加门店相关组件注册

--- src/main.js
+++ src/main.js
@@ -16,6 +16,13 @@ import addGoods from './components/storeManage/goodsManage/addGoods.vue';
 import addPhoto from './components/storeManage/goodsManage/addPhoto.vue';
 import goods from './components/storeManage/goodsManage/goods.vue';
 
+//门店商品
+import Shop from './components/storeManage/shop/shop';
+import AddShop from './components/storeManage/shop/addShop';
+import ServiceForShop from './components/storeManage/shop/serviceForShop';
+import GoodsForShop from './components/storeManage/shop/goodsForShop';
+import AddGoodsForShop from './components/storeManage/shop/addGoodsForShop';
+
 import store from './store';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -27,7 +34,15 @@ Vue.component('editUser',editUser);
 Vue.component('editOrder',editOrder);
 Vue.component('serviceForm',serviceForm);
 Vue.component('addGoods',addGoods);
-Vue.component('addPhoto',addPhoto)
+Vue.component('addPhoto',addPhoto);
+
+Vue.component("Shop",Shop);
+Vue.component("AddShop",AddShop);
+Vue.component("ServiceForShop",ServiceForShop);
+Vue.component("GoodsForShop",GoodsForShop);
+Vue.component("AddGoodsForShop",AddGoodsForShop);
+
+
 import router from './router'
 
 Vue.config.productionTip = false
